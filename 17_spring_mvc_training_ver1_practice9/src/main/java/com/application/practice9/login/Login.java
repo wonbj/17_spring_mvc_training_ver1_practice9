6b4c44a1b25ee717a7875b02a0ef67319073fe70
kai97 <kai97@192.168.20.15>
2025-02-11 19:28:14 +0900

    hihi

---
+++ 17_spring_mvc_training_ver1_practice9/src/main/java/com/application/practice9/login/Login.java
@@ -0,0 +1,16 @@
+package com.application.practice9.login;
+
+import java.util.List;
+
+import com.application.practice9.post.dto.PostDTO;
+
+public interface Login {
+
+	public List<PostDTO> getPostList();
+	public void createPost(PostDTO postDTO);
+	public Object getPostDetail(long postId, boolean isUpdateReadCnt);
+	public boolean checkAuthenticationUser(PostDTO postDTO);
+	public void updatePost(PostDTO postDTO);
+	public void deletePost(long postId);
+
+}
